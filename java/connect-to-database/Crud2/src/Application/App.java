e45b62184c71aa2b28e5f31232d9ec892696ac3a
Leozin89Python <leozin069@gmail.com>
2021-01-02 15:14:30 -0300

    exercicios-java-mysql-workbench

--- java/connect-to-database/Crud2/src/Application/App.java
+++ java/connect-to-database/Crud2/src/Application/App.java
@@ -1,36 +1,48 @@
 package Application;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import db.DB;
 import db.DbException;
 
 public class App {
 
-	public static void main(String[] args) {
-		Connection con = DB.getConection();
-		Statement st = null;
-		ResultSet rs = null;
+	public static void main(String[] args) throws ParseException {
+	
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Connection con = null;
+		PreparedStatement st =null;
 		try {
 			con = DB.getConection();
 			
-			st = con.createStatement();
-			rs = st.executeQuery("select * from department");
+			st = con.prepareStatement(
+					"INSERT INTO seller"
+					+"(Name,Email,BirthDate,BaseSalary,DepartmentId)"
+					+"values"
+					+"(?,?,?,?,?)");
 			
-		while(rs.next()) {
-			System.out.println(rs.getInt("Id") + "," + rs.getString("Name"));
-		}
+			st.setString(1,"Carl Urban");
+			st.setString(2, "carl@eu.com");
+			st.setDate(3, new java.sql.Date(sdf.parse("22/03/1987").getTime()));
+			st.setDouble(4, 2354.35);
+			st.setInt(5, 4);
+			
+			int rowsAffected = st.executeUpdate();
+			
+			System.out.println("Done! rows affected" + "," + rowsAffected);
 			
 		}catch(SQLException e) {
 			throw new DbException(e.getMessage());
+		}catch(ParseException e) {
+			e.printStackTrace();
 		}finally {
-			DB.closeResultSet(rs);
-			DB.closeStatement(st);
 			DB.closeConnection();
-			
+			DB.closeStatement(st);
 		}
+		
 	}
 }
--- java/database/course.sql
+++ java/database/course.sql
@@ -1,5 +1,5 @@
 											
-										=== codigos de colsulta ===
+											=== CRUD SQL ===
                                         
                                         
                                         
@@ -8,8 +8,23 @@
 drop table department;
 drop table seller;
 
+insert into seller values(null,'Erica Souza','Ericalinda@gmail.com','1989-04-21 00:00:00',2456,3);
+insert into seller values(null,'Fernanda Sena','nandalindamenina@gmail.com','1995-06-24 00:00:00',2456,3);
+insert into seller values(null,'Renata Souza','renacris@gmail.com','1981-07-15 00:00:00',2456,3);
+insert into seller values(null,'Rodrigo Mendes','menderr@gmail.com','1992-02-01 00:00:00',2014,2);
+insert into seller values(null,'Miguel Coelho','miguelito@outlook.com','1997-05-21 00:00:00',998,1);
+insert into seller values(null,'Renata Souza','renacris@gmail.com','1981-07-15 00:00:00',2456,3);
+insert into seller values(null,'Renata Souza','renacris@gmail.com','1981-07-15 00:00:00',2456,3);
+
 select * from department;
 select * from seller;
+
+
+delete from seller where id = '8';
+
+update seller set Name  ='Debora Block' where id ='14'
+update seller set Email ='deboraagata@eu.com' where id ='14'
+
 
 select s.Name ,s.Email , d.Name 
 from department d
@@ -57,7 +72,6 @@ select * from  vdepartment;
 create view vseller as select * from seller;
 
 select * from  vseller;
-
 
 
 
