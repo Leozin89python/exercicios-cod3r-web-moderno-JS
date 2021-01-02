7460dd369956dc3d78d1828f8c915a44b27c1478
Leozin89Python <leozin069@gmail.com>
2021-01-01 22:25:53 -0300

    conhecimento-diverso

---
+++ java/connect-to-database/Test/src/Application/App.java
@@ -0,0 +1,14 @@
+package Application;
+
+import java.sql.Connection;
+import db.DB;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		
+		Connection con = DB.getConection();
+		DB.closeConnection();
+	}
+}
